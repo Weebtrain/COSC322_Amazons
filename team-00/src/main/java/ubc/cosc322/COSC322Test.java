d1d4d7de2108e6daaf1f0166b6ae4845d0d47451
Weebtrain <cmaranda2001@gmail.com>
2025-03-20 11:23:14 -0700

    Actually fixed things now

--- team-00/src/main/java/ubc/cosc322/COSC322Test.java
+++ team-00/src/main/java/ubc/cosc322/COSC322Test.java
@@ -39,7 +39,7 @@ public class COSC322Test extends GamePlayer{
      * @param args for name and passwd (current, any string would work)
      */
     public static void main(String[] args) {				 
-    	COSC322Test player = new COSC322Test("WeebTrain", "COSC322");	//Username display in server, password into server
+    	COSC322Test player = new COSC322Test("WeebTrain2", "COSC322");	//Username display in server, password into server
     	if(player.getGameGUI() == null) {
     		player.Go();
     	}
@@ -172,10 +172,10 @@ public class COSC322Test extends GamePlayer{
 		System.out.println("Black Player: " + (msgDetails.get(AmazonsGameMessage.PLAYER_BLACK)));
 		System.out.println("White Player: " + (msgDetails.get(AmazonsGameMessage.PLAYER_WHITE)));
 		if (((String)msgDetails.get(AmazonsGameMessage.PLAYER_BLACK)).equals(userName)) {
-			queenIdentity = 1;
+			queenIdentity = 2;
 			//Starts a human player or the ai depending on indication
 			RunPlayer();
-		} else queenIdentity = 2;
+		} else queenIdentity = 1;
 	}
 
 	void RunPlayer () {
