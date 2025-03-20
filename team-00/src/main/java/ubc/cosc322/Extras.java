e2dace0b148360d7ebe1d17e266d43ec26972fbd
Weebtrain <cmaranda2001@gmail.com>
2025-03-20 11:28:09 -0700

    Fixing API issues

--- team-00/src/main/java/ubc/cosc322/Extras.java
+++ team-00/src/main/java/ubc/cosc322/Extras.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Extras {
     public static ArrayList<Integer> arrayToArrayList (byte[] array) {
         ArrayList<Integer> returnList = new ArrayList<>(2);
-        returnList.add(0, (int)(9-array[0]));
+        returnList.add(0, (int)(11-array[0]));
         returnList.add(1, (int)array[1]);
         return returnList;
     }
